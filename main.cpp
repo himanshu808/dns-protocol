a975d83d6c1c056306c7b200f33f742702058838
himanshu808 <himanshushah.alt@gmail.com>
2023-03-08 13:31:28 -0800

    refactoring code

--- main.cpp
+++ main.cpp
@@ -10,29 +10,27 @@ void readFile(std::string &fileName, BytePacketBuffer &buffer) {
     file.read((char *)&buffer.buf[0], sizeof(buffer.buf));
 }
 
+bool validateInput(std::string recordType, QueryType::QueryTypeEnum qtype) {
+    if (!std::all_of(recordType.begin(), recordType.end(), ::isdigit)) {
+        std::cout << "<record-type> should only contain digits" << std::endl;
+        return false;
+    }
 
-int main(int argc, const char **argv) {
-    if (argc != 4) {
-        std::cout << "Usage: <executable-name> <lookup-domain> <record-type> <dns-resolver-ip>" << std::endl;
-        return 0;
-    } else {
-        std::string recordType = std::string(argv[2]);
-        if (!std::all_of(recordType.begin(), recordType.end(), ::isdigit)) {
-            std::cout << "<record-type> should only contain digits" << std::endl;
-            return 0;
-        }
-    };
-
-
-    QueryType::QueryTypeEnum qtype = QueryType::toEnum(std::stoi(argv[2]));
     if (qtype == QueryType::QueryTypeEnum::UNKNOWN) {
         std::cout << "<record-type> can be either 1, 2, 5, 15, 28" << std::endl;
-        return 0;
+        return false;
     }
-    std::string domain = std::string(argv[1]) + ".";
 
-    std::string servaddr = std::string(argv[3]);
+    return true;
+}
+
+void performLookup(std::string &domain, std::string &servaddr, std::string &recordType) {
+    QueryType::QueryTypeEnum qtype = QueryType::toEnum(std::stoi(recordType));
+    if (!validateInput(recordType, qtype)) return;
+
     int servport = 53;  // DNS UDP port
+
+    domain = domain + ".";
 
     UDPClient udp(servaddr, servport);
     DnsPacket packet;
@@ -54,6 +52,19 @@ int main(int argc, const char **argv) {
     respacket.packetFromBuffer(resbuffer);
 
     std::cout << respacket << std::endl;
+}
+
+
+int main(int argc, const char **argv) {
+    if (argc != 4) {
+        std::cout << "Usage: <executable-name> <lookup-domain> <record-type> <dns-resolver-ip>" << std::endl;
+        return 0;
+    }
+
+    std::string domain = std::string(argv[1]);
+    std::string servaddr = std::string(argv[3]);
+    std::string recordType = std::string(argv[2]);
+    performLookup(domain, servaddr, recordType);
 
     return 0;
 }
